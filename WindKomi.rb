8c950bc0b329396741256ae0d03435add446f206
Thomas Salmon <th_s@hotmail.fr>
2012-07-07 22:46:35 +0200

    add size of file

--- WindKomi.rb
+++ WindKomi.rb
@@ -13,7 +13,13 @@ def my_ls(name,extensions={})
 	for t in Dir.entries(name)
 		if(t =~ /^\w+(\.\w+)+$/)
 			for i in t.split(".")[1,t.split(".").length]
-				extensions[i]==nil ? extensions[i]=1 : extensions[i]+=1
+				if extensions[i] == nil 
+					extensions[i] = 1
+					extensions[i + " taille"] = 0
+				else
+					extensions[i]+=1
+					extensions[i + " taille"]+= t.size
+				end
 			end
 		end
 		if !(t =~ /\.|\.\./) && File.directory?(name+"/"+t)
@@ -23,9 +29,11 @@ def my_ls(name,extensions={})
 	return extensions
 end
 
-def erreur(message)
-	puts message
+def lire(message)
+	puts "Nom\tNombre\tTaille"
+	message.each { |a,b| print (a.split.length == 1) ? "#{a.split[0]}\t#{b}" : "\t#{b}\n"}
+	return ""
 end
 
-print (ARGV.length > 0) ? (File.directory?(ARGV[0]) ? my_ls(ARGV[0]) : "Pas un repertoire" ):  "mettre un argument"
+print (ARGV.length > 0) ? (File.directory?(ARGV[0]) ? lire(my_ls(ARGV[0])) : "Pas un repertoire" ):  "mettre un argument"
 puts ""
