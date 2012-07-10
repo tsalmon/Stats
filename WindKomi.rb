d7b5f1705a0e3cf03e8248a3120815b3a6ea6eb1
Thomas Salmon <th_s@hotmail.fr>
2012-07-10 11:04:29 +0200

    lire réécrit

--- WindKomi.rb
+++ WindKomi.rb
@@ -6,16 +6,16 @@ def my_ls(name, extensions={})
 					extensions[i + " nombre"] = 1
 					extensions[i + " taille"] = File.stat(name + "/" + t).size
 					
+					extensions[i + " recentdate"] = File.stat(name + "/" + t).ctime
 					extensions[i + " recentnom"] = t
-					extensions[i + " recentdate"] = File.stat(name + "/" + t).ctime 
 					extensions[i + " recentchemin"] = name + "/" + t 
 					
-					extensions[i + " vieuxnom"] = t
-					extensions[i + " vieuxdate"] = File.stat(name + "/" + t).ctime
+					extensions[i + " vieuxnom"] = File.stat(name + "/" + t).ctime
+					extensions[i + " vieuxdate"] = t
 					extensions[i + " vieuxchemin"] = name + "/" + t
 					
-					extensions[i + " lourdnom"] = t
 					extensions[i + " lourdpoid"] = File.stat(name + "/" + t).size
+					extensions[i + " lourdnom"] = t
 					extensions[i + " lourdchemin"] = name + "/" + t 
 					
 				else	
@@ -47,11 +47,26 @@ end
 def lire(message)
 	i = 11
 	message.each do |k,v|
-		if(i%11==0)
+		if(i == 11)
 			i = 0
-			puts k.split()[0]
+			print k.split()[0]
 		end
-			puts "\t"+ k.split()[1].to_s + " : " + v.to_s
+
+		if(i == 0)
+			print "(" + v.to_s + ")\n"
+		elsif (i == 1)
+			puts "\ttaille : " + v.to_s 
+		elsif ( ( i == 2 ) || ( i == 5 ) )
+			print "\t" + ( i == 2 ? "plus recent (" : "plus vieux (")  + v.to_s + ") : " 
+		elsif ( ( ( i == 3 ) || ( i == 4) ) || ( ( i == 6 ) || ( i == 7 ) ) )
+			print v.to_s + ( ( ( i == 4 ) || ( i == 7 ) ) ? "\n" : " " )
+		elsif ( i == 8 )
+			print "\tplus lourd (" + v.to_s +  ") :"
+		elsif ( i == 9 )
+			print v.to_s + " "
+		else
+			print v.to_s + "\n"
+		end
 		i += 1
 	end
 	return nil
